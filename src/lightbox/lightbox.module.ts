6c100c5cbe2e78984cc641db7fbd2bd1fe09b1b2
dcmorales1987 <dcmorales1987@gmail.com>
2018-03-30 01:43:52 -0400

    Add LightboxService

--- src/lightbox/lightbox.module.ts
+++ src/lightbox/lightbox.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { LightboxComponent } from './components/lightbox/lightbox.component';
 import { LightboxImgDirective } from './directives/lightbox-img/lightbox-img.directive';
 import { LightboxVideoDirective } from './directives/lightbox-video/lightbox-video.directive';
+import { LightboxService } from './services/lightbox.service';
 
 @NgModule({
     declarations: [
@@ -9,7 +10,8 @@ import { LightboxVideoDirective } from './directives/lightbox-video/lightbox-vid
         LightboxImgDirective,
         LightboxVideoDirective
     ],
-    imports: [
+    providers: [
+        LightboxService
     ]
 })
 export class LightboxModule { }
