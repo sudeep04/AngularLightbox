7ac74b0c2f4c54ac8dd0f6d33a489d0b830088e0
dcmorales1987 <dcmorales1987@gmail.com>
2018-04-08 07:36:03 -0400

    update navigation controls toggle

--- src/lightbox/components/lightbox/lightbox.component.ts
+++ src/lightbox/components/lightbox/lightbox.component.ts
@@ -183,7 +183,23 @@ export class LightboxComponent{
     }
 
     public onToggle(): void {
+
         this.header.toggle();
+
+        if (this.navigationNextAnimator == 'show') {
+
+            this.navigationNextAnimator = 'hide';
+        } else {
+            
+            this.navigationNextAnimator = 'show';
+        }
+        if (this.navigationPreviousAnimator == 'show') {
+            
+            this.navigationPreviousAnimator = 'hide';
+        } else {
+            
+            this.navigationPreviousAnimator = 'show';
+        }
     }
 
     public next() {
@@ -252,141 +268,4 @@ export class LightboxComponent{
             this.hasNext = false;
         }
     }
-    
-
-
-    // public fadeAnimator: 'show' | 'hide' = 'hide';
-
-    // public hasNext: boolean;
-
-    // public hasPrevious: boolean;
-
-    // @ViewChild('_next') private _next: LightboxButtonComponent;
-    // @ViewChild('_previous') private _previous: LightboxButtonComponent;
-
-    // private _state: BehaviorSubject<'closed' | 'opened'> = new BehaviorSubject<'closed' | 'opened'>('closed');
-
-
-
-    // public get state(): 'closed' | 'opened' {
-
-    //     return this._state.getValue();
-    // }
-
-    // public get $state(): Observable<'closed' | 'opened'> {
-
-    //     return this._state.asObservable();
-    // }
-
-    
-
-    // public open(activeItem: ILightboxItemComponent, items: ILightboxItemComponent[]) {
-    //     activeItem.animateHostFixed();
-    //     activeItem.animateOrigin().done(()=>{
-    //         activeItem.animateCenter();
-    //     });
-    //     this._pointerEvents = 'auto';
-    //     this.header.open();
-    //     this.activeItem = activeItem;
-    //     this.items = items;
-    //     // this.items = items;
-    //     // this.activeItem = this.items.find((x) => x.id === activeItem);
-    //     // const activeItemIndex = this.items.indexOf(this.activeItem);
-
-    //     // // this.items.forEach((item) => {
-
-    //     // //     const itemIndex = this.items.indexOf(item);
-
-    //     // //     if (itemIndex < activeItemIndex) {
-    //     // //         item.position = 'left';
-    //     // //     }
-    //     // //     if (itemIndex === activeItemIndex) {
-    //     // //         item.position = 'origin';
-    //     // //     }
-    //     // //     if (itemIndex > activeItemIndex) {
-    //     // //         item.position = 'right';
-    //     // //     }
-    //     // // });
-
-    //     // this._checkNavigation();
-    //     // this._pointerEvents = 'auto';
-    //     // this.fadeAnimator = 'show';
-    //     // this._previous.show();
-    //     // this._next.show();
-    //     // this.headerShowAnimator = 'show';
-    // }
-
-    // public close() {
-
-    //     this._pointerEvents = 'none';
-    //     this.header.close();
-    //     this.items.map((item)=> {item.animateNull()});
-    // }
-
-    // public next() {
-
-    //     // const activeItemIndex = this.items.indexOf(this.activeItem);
-
-    //     // if (activeItemIndex >= 0 && activeItemIndex < this.items.length - 1) {
-    //     //     this._itemsRef.toArray()[activeItemIndex].changePosition('left');
-    //     //     this.activeItem = this.items[activeItemIndex + 1];
-    //     //     this._itemsRef.toArray()[activeItemIndex + 1].changePosition('center');
-    //     // }
-    //     // this._checkNavigation();
-    // }
-
-    // public previous() {
-
-    //     // const activeItemIndex = this.items.indexOf(this.activeItem);
-
-    //     // if (activeItemIndex > 0) {
-    //     //     this._itemsRef.toArray()[activeItemIndex].changePosition('right');
-    //     //     this.activeItem = this.items[activeItemIndex - 1];
-    //     //     this._itemsRef.toArray()[activeItemIndex - 1].changePosition('center');
-    //     // }
-    //     // this._checkNavigation();
-    // }
-
-    // public toggle() {
-
-    //     this.header.toggle();
-    // }
-
-    // public startFadeAnimator(event: AnimationEvent) {
-
-    //     // if (event.fromState === 'hide') {
-
-    //     //     const activeItemIndex = this.items.indexOf(this.activeItem);
-
-    //     //     this._state.next('opened');
-    //     //     this._itemsRef.toArray()[activeItemIndex].changePosition('center');
-    //     // }
-    // }
-
-    // public doneFadeAnimator(event: AnimationEvent) {
-
-    //     if (event.toState === 'hide') {
-    //         this._pointerEvents = 'none';
-    //         this._state.next('closed');
-    //         this.items = [];
-    //         this.activeItem = null;
-    //     }
-    // }
-
-    // private _checkNavigation() {
-
-    //     const activeItemIndex = this.items.indexOf(this.activeItem);
-
-    //     if (activeItemIndex > 0) {
-    //         this.hasPrevious = true;
-    //     } else {
-    //         this.hasPrevious = false;
-    //     }
-
-    //     if (activeItemIndex >= 0 && activeItemIndex < this.items.length - 1) {
-    //         this.hasNext = true;
-    //     } else {
-    //         this.hasNext = false;
-    //     }
-    // }
 }
